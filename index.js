2f4fb934c521b7936dcd4326b78f74d5a7d8b861
Priyashan Gimantha <120010882+EroseEmrys@users.noreply.github.com>
2024-10-04 23:18:55 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-let key;
+function search() {
+  alert("Searching...");
+  let movie = document.getElementById("searchBar").value;
+  console.log(movie);
+
+  const htmlRequest = new XMLHttpRequest();
+  htmlRequest.open("GET", "http://www.omdbapi.com/?apikey=1597bec9&t=" + movie);
+  htmlRequest.responseType = "json";
+  htmlRequest.send();
+
+  htmlRequest.onload = function () {
+    if (htmlRequest.status == 200) {
+      let response = htmlRequest.response;
+      console.log(response);
+
+      if (response.Response === "True") {
+        document.getElementById("content").innerHTML = `
+            <h2>${response.Title}</h2>
+            <p>${response.Plot}</p>
+            <img src="${response.Poster}" alt="Movie Poster">
+          `;
+      } else {
+        document.getElementById(
+          "content"
+        ).innerHTML = `<p>Movie not found!</p>`;
+      }
+    } else {
+      document.getElementById(
+        "content"
+      ).innerHTML = `<p>Error fetching data!</p>`;
+    }
+  };
+}
